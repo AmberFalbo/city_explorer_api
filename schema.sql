d20d6561328f7dc4fe9153b289a697dd13987f6e
Amber Falbo <amberannfalbo@gmail.com>
2020-07-16 16:09:41 -0700

    got movies up

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE locations (
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
   latitude NUMERIC(10, 7),
-  longitude NUMERIC(10, 7),
+  longitude NUMERIC(10, 7)
 );
 
 INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ('Seattle', 'Seattle, King County, Washington, USA', '47.6173', '122.3195');
